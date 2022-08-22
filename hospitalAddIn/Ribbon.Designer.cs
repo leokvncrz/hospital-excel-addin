50412baf956df6a3c1463e791be28190cfa71391
Zero <leokevin.cruz@gmail.com>
2022-08-23 07:00:45 +0800

    Add project files.

---
+++ hospitalAddIn/Ribbon.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace hospitalAddIn
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.convertBtn = this.Factory.CreateRibbonButton();
+            this.highlightBtn = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "Hospital";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.convertBtn);
+            this.group1.Items.Add(this.separator1);
+            this.group1.Items.Add(this.highlightBtn);
+            this.group1.Label = " ";
+            this.group1.Name = "group1";
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.convertBtn.Label = "Convert ICD and CPT codes";
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.OfficeImageId = "ConvertTableToText";
+            this.convertBtn.ShowImage = true;
+            this.convertBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.convertBtn_Click);
+            // 
+            // highlightBtn
+            // 
+            this.highlightBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.highlightBtn.Label = "Highlight Keywords";
+            this.highlightBtn.Name = "highlightBtn";
+            this.highlightBtn.OfficeImageId = "ConditionalFormattingHighlightCellsMenu";
+            this.highlightBtn.ShowImage = true;
+            this.highlightBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.highlightBtn_Click);
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton convertBtn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton highlightBtn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ hospitalAddIn/Ribbon.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+
+namespace hospitalAddIn
+{
+    public partial class Ribbon
+    {
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void convertBtn_Click(object sender, RibbonControlEventArgs e) {
+            MessageBox.Show("Convert Clicked");
+        }
+        private void highlightBtn_Click(object sender, RibbonControlEventArgs e) {
+            MessageBox.Show("Highlight Clicked");
+        }
+    }
+}
